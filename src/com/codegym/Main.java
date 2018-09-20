c7129b4a6ae5f3d7e8266c6b7d0150461465f744
nguyenkhanhtung <tungnk0711@gmail.com>
2018-09-20 17:36:31 +0700

    commit project

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,20 @@
+package com.codegym;
+
+public class Main {
+    private static EmailExample emailExample;
+    public static final String[] validEmail = new String[]{"a@gmail.com", "ab@yahoo.com", "abc@hotmail.com"};
+    public static final String[] invalidEmail = new String[]{"@gmail.com", "ab@gmail.", "@#abc@gmail.com"};
+
+    public static void main(String[] args) {
+        // write your code here
+        emailExample = new EmailExample();
+        for (String email : validEmail) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email + " is valid: " + isvalid);
+        }
+        for (String email : invalidEmail) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email + " is valid: " + isvalid);
+        }
+    }
+}
